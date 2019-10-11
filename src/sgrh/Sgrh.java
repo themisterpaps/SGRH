4d52e8e6d721deebd7c25d61623e6cb81cf6f87b
Henrique <henriquesmateus3119@gmail.com>
2019-10-11 09:40:31 +0200

    remove issue

--- src/sgrh/Sgrh.java
+++
@@ -1,8 +0,0 @@
-package sgrh;
-import java.io.*;
-public class Sgrh {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-       Menu m = new Menu();
-       m.menuPrincipal();
-    }    
-}
