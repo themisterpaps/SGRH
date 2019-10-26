9b8b2baffe955470684689ea374daf79367afbad
henrique <henriqueChigumane@outlook.com>
2019-10-26 19:19:58 +0200

    Add logn in changes

---
+++ src/DAO/ModuloConexao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.sql.*;
+
+public class ModuloConexao {
+
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/sgrh3?useTimezone=true&serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+            
+        }
+    }
+}
