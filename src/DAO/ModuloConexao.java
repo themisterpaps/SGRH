63fed2929479dc360a23cb9ed94974a0a771f616
Henrique <henriquechigumane@outlook.com>
2019-10-31 05:44:59 +0200

    Update ModuloConexao.java

--- src/DAO/ModuloConexao.java
+++ src/DAO/ModuloConexao.java
@@ -1,22 +1,27 @@
 package DAO;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ModuloConexao {
 
     public static Connection conector() {
-        java.sql.Connection conexao = null;
-        String driver = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/sgrh3?useTimezone=true&serverTimezone=UTC";
-        String user = "root";
-        String password = "";
+ 
+            java.sql.Connection conexao = null;
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/sgrh3?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "";
+            
         try {
             Class.forName(driver);
-            conexao = DriverManager.getConnection(url, user, password);
-            return conexao;
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ModuloConexao.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-}
+        try {
+            conexao = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(ModuloConexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return conexao; 
+}}
